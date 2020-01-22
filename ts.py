ea3a5bdcd9d2911e8d314423b300fff34fd07c6b
ivan.ivanov <ivanov@example.com>
2020-01-22 17:06:37 +0300

    All worl, base func compl. Start create User class

---
+++ ts.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+from tkinter import simpledialog
+from random import randint
+
+WIDTH = 300
+HEIGHT = 200
+
+class Ball:
+    def __init__(self):
+        self.R = randint(20, 50)
+        self.x = randint(1, WIDTH - self.R)
+        self.y = randint(1, HEIGHT - self.R)
+        self.dx, self.dy = (+2, +3)
+        self.points = {'red': 20, 'orange': 32, 'yellow': 50, 'green': 20, 'blue': 100}
+        self.pick_color = list(self.points.keys())[randint(0,4)]
+        self.ball_id = canvas.create_oval(self.x - self.R, self.y - self.R, self.x + self.R, self.y + self.R, fill= self.pick_color )
+
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+        if self.x + self.R > WIDTH or self.x - self.R <= 0:
+            self.dx = -self.dx
+        if self.y + self.R > HEIGHT or self.y - self.R <= 0:
+            self.dy = -self.dy
+
+    def show(self):
+        canvas.move(self.ball_id, self.dx, self.dy)
+
+
+class User:
+    def __init__(self, name , points):
+        self.name = name
+        self.points = points
+
+    def remember_user(self):
+        f = open('user.txt', 'a')  # toDO попробовать pickle или json
+        data = str(self.name) + ":" + self.points
+        f.write(data + '\n')
+        f.close()
+    def find_user(self):
+        f = open('user.txt', 'r')
+        for line in f:
+            print(line)    # дописать тут проверку на созданного пользователя и переписывать ему очки в файл
+
+def canvas_click_handler(event):
+    # print("x=", event.x , 'y=' , event.y)
+    for ball in balls:
+        # print(ball.x)
+        if (((ball.x - event.x) ** 2 ) ** 0.5)+(((ball.y - event.y) ** 2) ** 0.5) <= ball.R: # По теореме пифогора расстояние между 2 точками
+            print(ball.pick_color)
+            canvas.delete(ball.ball_id)
+            del ball # TODO проверить удаляет ли экземпляр класса
+            break
+
+
+def tick():
+    for ball in balls:
+        ball.move()
+        ball.show()
+    root.after(50, tick)
+
+
+def main():
+    global root, canvas, balls, name
+    # application_window = tk.Tk()
+    # name = simpledialog.askstring("Input", "What is your first name?", parent=application_window)
+    # print(name)
+
+    root = tk.Tk()
+    name = simpledialog.askstring("Input", "What is your first name?", parent=root)
+    root.geometry(str(WIDTH) + "x" + str(HEIGHT))
+    canvas = tk.Canvas(root)
+    canvas.pack(anchor="nw", fill=tk.BOTH)
+    canvas.bind("<Button-1>", canvas_click_handler)
+    balls = [Ball() for i in range(5)]
+    tick()
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    #main()
+    us = User('ivwqeqan', '220')
+    us2 = User('sam', '10')
+    us.remember_user()
+    us.find_user()
+    us2.remember_user()
+    us2.find_user()
+
+
+
---
+++ view2.py
@@ -0,0 +1,4 @@
+def foo(a):
+	return print('foo2' + ' ' + str(a))
+
+print('view2', __name__)	
